04da0afea28f74b71a14343fd8c764bf8d2a38e6
polseane9067 <polseane9067@my.uwstout.edu>
2015-02-25 15:03:32 -0600

    Level 1 ish

---
+++ GoingNuts/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+    public GameObject spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = spawnPoint.transform.position;
+            other.gameObject.transform.rotation = spawnPoint.transform.rotation;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.transform.position = spawnPoint.transform.position;
+            other.gameObject.transform.rotation = spawnPoint.transform.rotation;
+        }
+    }
+}
--- GoingNuts/Assets/Scripts/PickupScript.cs
+++ GoingNuts/Assets/Scripts/PickupScript.cs
@@ -3,6 +3,46 @@ using System.Collections;
 
 public class PickupScript : MonoBehaviour {
 
+    public Transform[] points;
+    private int Currentpoint;
+    public float moveSpeed;
+    float startMove;
+    float startTime;
+    GameObject acorn;
+
+    void Start()
+    {
+        startTime = Time.time;
+        startMove = Random.Range(0, 3);
+        Currentpoint = 0;
+
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.name == "Acorn")
+            {
+                acorn = child.gameObject;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Time.time - startTime >= startMove)
+        {
+            if (acorn.transform.position == points[Currentpoint].position)
+            {
+                Currentpoint++;
+            }
+
+            if (Currentpoint >= points.Length)
+            {
+                Currentpoint = 0;
+            }
+
+            acorn.transform.position = Vector3.MoveTowards(acorn.transform.position, points[Currentpoint].position, moveSpeed * Time.deltaTime);
+        }
+    }
+
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject.tag == "Player")
