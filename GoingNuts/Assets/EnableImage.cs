ef984acde9d9276ba175f04d4625492134a1692e
Ashanden <mackerta6548@my.uwstout.edu>
2015-04-03 16:02:09 -0500

    Organization and MOAR BIRDS
    Birds in third area probably need to be adjusted more.

---
+++ GoingNuts/Assets/EnableImage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnableImage : MonoBehaviour {
+
+	private Image image;
+
+	// Use this for initialization
+	void Start () {
+		GameObject fader = GameObject.Find("Fader");
+		image = fader.GetComponent<Image> ();
+		image.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
