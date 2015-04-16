e01ef994c9aa669acb145474b4143f51cb2b66ab
polseane9067 <polseane9067@my.uwstout.edu>
2015-04-16 13:59:55 -0500

    New stuffs

---
+++ GoingNuts/Assets/Scripts/Blah.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blah : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] Acorns = GameObject.FindGameObjectsWithTag("Pickup");
+        foreach (GameObject acorn in Acorns)
+        {
+            acorn.gameObject.transform.position = new Vector3(-39.21728f, 5.456894f, -5.317715f);
+        }
+        print("Acorns: " + Acorns.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
