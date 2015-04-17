71e1e5d6b032d8b753cf1abb60fcc6169699f0e1
Ashanden <mackerta6548@my.uwstout.edu>
2015-04-17 11:45:48 -0500

    stuff

---
+++ GoingNuts/Assets/ChopperSoundRandomStart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChopperSoundRandomStart : MonoBehaviour {
+	private float randNum; 
+	private float currentTime;
+	private bool played = false;
+	// Use this for initialization
+	void Start () 
+	{
+
+		randNum = Random.Range(0F, 10.0F)/10;
+		currentTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= currentTime + randNum && played == false)
+		{
+			played = true;
+			audio.Play();
+			print (currentTime);
+			print (Time.time);
+			print (randNum);
+		}
+	
+	}
+}
