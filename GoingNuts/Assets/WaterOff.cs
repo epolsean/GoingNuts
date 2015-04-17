b3018d63b008fd89e08ea7dac7e7a2824c842172
Ashanden <mackerta6548@my.uwstout.edu>
2015-04-17 15:54:04 -0500

    water soooooound

---
+++ GoingNuts/Assets/WaterOff.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterOff : MonoBehaviour {
+	public AudioSource water1;
+	public AudioSource water2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			water1.volume = 0;
+			water2.volume = 0;
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			water1.volume = 1;
+			water2.volume = 1;
+		}
+	}
+}
